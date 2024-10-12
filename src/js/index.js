b4a171b88dbae7d16e2824fa9595771d50081b46
Jimena <jparatge@gmail.com>
2024-10-12 19:00:34 +0000

    finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -7,15 +7,24 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-import SecondsCounter from "./secondsCounter.jsx";
+import { Card, SecondsCounter }  from "./secondsCounter.jsx";
+
 
 let seconds = 0
 
 //render your react application
-let app= ReactDOM.createRoot(document.getElementById('app'))
-app.render(<SecondsCounter counter={seconds} />);
+let app = ReactDOM.createRoot(document.getElementById('app'))
 
-setInterval (()=> {
+setInterval(() => { 
     seconds++;
-    app.render(<SecondsCounter counter={seconds} />);   
-} ,1000) 
+app.render(<>
+    <SecondsCounter counter={seconds} />
+    <div>
+        <input onChange={(event) => {
+            seconds = event.target.value;
+        }}
+     
+            placeholder="ingresar" type="number" />
+    </div>  
+
+</>);} ,1000);
--- src/js/secondsCounter.jsx
+++ src/js/secondsCounter.jsx
@@ -3,27 +3,25 @@ import React from "react";
 const Card = (props) => {
     return (
         <div class="card text-bg-dark mb-3 m-1">
-                <div class="card-body">
-                    <h5 class="card-title">{props.digito}</h5>
-                </div>
+            <div class="card-body">
+                <h5 class="card-title">{props.digito}</h5>
+            </div>
         </div>
-)}
-const SecondsCounter = (props) => { 
+    )
+}
+const SecondsCounter = (props) => {
     return (
-        <div className="d-flex" >
-             <Card digito={Math.floor (props.counter/10000 %10)}/>
-             <Card digito={Math.floor (props.counter/10000 %10)}/>
-             <Card digito={Math.floor (props.counter/1000 %10) }/> 
-             <Card digito={Math.floor (props.counter/100 %10)}/> 
-             <Card digito={Math.floor(props.counter/10 %10)}/> 
-             <Card digito={props.counter %10}/>    
-
-
+        <div className="d-flex col-md-4" >
+            <div className="card card-body text-bg-dark mb-3 m-1 ">
+                <i className="far fa-clock my-1"></i>
+            </div>
+            <Card digito={Math.floor(props.counter / 10000 % 10)} />
+            <Card digito={Math.floor(props.counter / 10000 % 10)} />
+            <Card digito={Math.floor(props.counter / 1000 % 10)} />
+            <Card digito={Math.floor(props.counter / 100 % 10)} />
+            <Card digito={Math.floor(props.counter / 10 % 10)} />
+            <Card digito={props.counter % 10} />
         </div>)
 }
 
-
-
-
-
-export default SecondsCounter;
+export { Card, SecondsCounter };
